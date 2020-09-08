4125da00d79789c465c016a8a2c988a4f1e2f12a
bnewman <bnewman@carbonite.com>
2020-09-08 19:50:27 -0400

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
